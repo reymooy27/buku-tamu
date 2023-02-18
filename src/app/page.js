d77ba4624f74a4eced7c89413931317cace7dee1
reymooy27 <gdrrey@gmail.com>
2023-02-19 01:59:49 +0800

    add dialog

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,16 @@
 'use client'
 
+import Dialog from '@/components/Dialog'
+import { getBaseUrl } from '@/utils/getBaseUrl'
+import { useDisclosure } from '@chakra-ui/react'
 import { Inter } from '@next/font/google'
 import Image from 'next/image'
 import { useState } from 'react'
-
 const inter = Inter({ subsets: ['latin'] , variable: 'inter'})
 
 export default function Home() {
+
+  const {onOpen} = useDisclosure()
 
   const [loading, setLoading] = useState(false)
 	const [disabled, setDisabled] = useState(false)
@@ -18,6 +22,11 @@ export default function Home() {
 		keterangan: ''
   })
 
+	function handleOpen(){
+		onOpen()
+		console.log('cliccck')
+	}
+
   async function handleSubmit(e){
 		e.preventDefault()
 
@@ -27,7 +36,7 @@ export default function Home() {
 
 		setLoading(true)
 		setDisabled(true)
-		await fetch('http://localhost:3000/api/create-tamu', {
+		await fetch(`${getBaseUrl()}/api/create-tamu`, {
 			method: 'POST',
 			body: JSON.stringify(input),
 		})
@@ -40,6 +49,7 @@ export default function Home() {
 				jenisKelamin: '',
 				keterangan: ''
 			})
+			
 			setLoading(false)
 			setDisabled(false)
 		})
@@ -57,78 +67,74 @@ export default function Home() {
   return (
 	<div className='flex h-full relative'>
 	{/* form */}
-		<div className='w-[0px] sm:w-[60%] mx-0 m-auto'>
+		<div className='w-[0px] sm:w-[60%] mx-0 m-auto' onClick={handleOpen}>
 			<Image 
 				className='mx-auto my-0' 
 				src={'/developer-team.svg'} 
-				width={600} 
-				height={600} 
+				width={500} 
+				height={500} 
 				alt='ilustration-developer-svg'
 			/>
 		</div>
 
-		<div className='form w-full sm:w-[40%] bg-white p-5 m-3 mr-5 rounded-[10px]'>
-			<h1 className='text-[28px] mb-[20px] text-center'>Selamat Datang</h1>
-			<form onSubmit={handleSubmit} className='flex flex-col gap-3 w-full'>
-					<input 
-						onChange={handleChange} 
-						value={input.nama} 
-						type="text" 
-						placeholder='Nama' 
-						name='nama' 
-						required
-					/>
-					<input 
-						onChange={handleChange} 
-						value={input.alamat} 
-						type="text" 
-						placeholder='Alamat' 
-						name='alamat'
-						required
-					/>
+		<div className='flex justify-center items-center h-full w-full sm:w-[40%] p-3 sm:pr-[40px]'>
+			<div className='form w-full bg-white p-5 rounded-[10px]'>
+				<h1 className='text-[28px] mb-[20px] text-center'>Selamat Datang</h1>
+				<form onSubmit={handleSubmit} className='flex flex-col gap-3 w-full'>
+						<input 
+							onChange={handleChange} 
+							value={input.nama} 
+							type="text" 
+							placeholder='Nama' 
+							name='nama' 
+							required
+						/>
+						<input 
+							onChange={handleChange} 
+							value={input.alamat} 
+							type="text" 
+							placeholder='Alamat' 
+							name='alamat'
+							required
+						/>
 
-				<input 
-					onChange={handleChange} 
-					value={input.hp} 
-					type="number" 
-					placeholder='No.Hp' 
-					name='hp' 
-					min={0}
-					minLength={11}
-					required
-				/>
-				<select 
-					onChange={handleChange} 
-					name='jenisKelamin' 
-					value={input.jenisKelamin}
-				>
-					<option value="Laki-Laki">Laki-Laki</option>
-					<option value="Perempuan">Perempuan</option>
-				</select>
-				<textarea 
-					onChange={handleChange} value={input.keterangan} 
-					name='keterangan' 
-					className='p-[10px] rounded-[10px] outline-none 
-										border border-solid border-[#d6d4d4] hover:border-black 
-										transition-all ease-in-out duration-300 focus:border-black' 
-					placeholder='Keterangan'
-				></textarea>
-				<button 
-					disabled={disabled} 
-					className={`${disabled ? 'opacity-80' : 'opacity-100'} btn-grad`}>
-						{loading ? 'Loading...' : 'Submit'}
-				</button>
-			</form>
+					<input 
+						onChange={handleChange} 
+						value={input.hp} 
+						type="number" 
+						placeholder='No.Hp' 
+						name='hp' 
+						min={0}
+						minLength={11}
+						required
+					/>
+					<select 
+						onChange={handleChange} 
+						name='jenisKelamin' 
+						value={input.jenisKelamin}
+					>
+						<option value="Laki-Laki">Laki-Laki</option>
+						<option value="Perempuan">Perempuan</option>
+					</select>
+					<textarea 
+						onChange={handleChange} value={input.keterangan} 
+						name='keterangan' 
+						className='p-[10px] rounded-[10px] outline-none 
+											border border-solid border-[#d6d4d4] hover:border-black 
+											transition-all ease-in-out duration-300 focus:border-black' 
+						placeholder='Keterangan'
+					></textarea>
+					<button 
+						disabled={disabled} 
+						className={`${disabled ? 'opacity-80' : 'opacity-100'} btn-grad`}>
+							{loading ? 'Loading...' : 'Submit'}
+					</button>
+				</form>
+			</div>
 		</div>
 
 
-{/* popup */}
-		{/* <div id='pop-up' className="absolute top-0 left-0 right-0 bottom-0 w-full h-full flex justify-center items-center bg-[#0000006c]">
-			<div className='w-[300px] h-[300px] rounded-[10px] bg-red-500'>
-					<button id="close">Close</button>
-			</div>
-		</div> */}
-
+		<Dialog/>
 	</div>
   )
 }
