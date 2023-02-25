ef788280ba41774e1346619dcbbf38ad8c175d96
reymooy27 <gdrrey@gmail.com>
2023-02-26 01:19:34 +0800

    add get tamu by date api

---
+++ src/pages/api/get-tamu-by-date.js
@@ -0,0 +1,26 @@
+import prisma from "@/server/db/client";
+
+export default async function handler(req, res) {
+
+  const date = req.body.date
+  
+  console.log('from server :' + date);
+
+  if (req.method === 'GET') {  
+    try{
+      const tamu = await prisma.tamu.findMany({
+        where:{
+          jamMasuk:{
+              gt: `${date}T00:00:00.000Z`,
+              lt: `${date}T23:59:59.999Z`,
+          }
+        },
+      })
+      res.status(200).json(tamu)
+    }catch(err){
+      throw err
+    }
+  } else {
+    res.status(404).json('Not Found')
+  }
+}
