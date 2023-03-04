fab70b03c1727ffe300a20c9e6d065df8303978c
reymooy27 <gdrrey@gmail.com>
2023-03-05 04:28:39 +0800

    prefetch

--- src/app/dashboard/head.js
+++ src/app/dashboard/head.js
@@ -2,6 +2,7 @@ export default function Head() {
   return (
     <>
       <title>Dashboard</title>
+      <link rel="preload" href="/api/data" as="fetch" crossorigin="anonymous"></link>
     </>
   )
 }
