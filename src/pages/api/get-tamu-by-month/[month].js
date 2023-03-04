6214fe421cb1dab2e3b4e53854e2d5bf0b86319a
reymooy27 <gdrrey@gmail.com>
2023-03-05 04:30:35 +0800

    get tamu by month api

---
+++ src/pages/api/get-tamu-by-month/[month].js
@@ -0,0 +1,30 @@
+import prisma from "@/server/db/client";
+
+export default async function handler(req, res) {
+
+  const month = parseInt(req.query.month, 10)
+
+  const thisYear = new Date().getFullYear()
+  
+  const start = new Date(thisYear, month, 1).toISOString()
+  const end = new Date(thisYear, month + 1, 0, 23, 59, 59, 999).toISOString()
+  console.log([start, end])
+
+  if (req.method === 'GET') {  
+    try{
+      const tamu = await prisma.tamu.findMany({
+        where:{
+          jamMasuk:{
+              gt: start,
+              lt: end,
+          },
+        },
+      })
+      res.status(200).json(tamu)
+    }catch(err){
+      throw err
+    }
+  } else {
+    res.status(404).json('Not Found')
+  }
+}
