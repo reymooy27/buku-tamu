8a163d7f679fecc7ca1dffda5631020afc0eaaf0
reymooy27 <gdrrey@gmail.com>
2023-03-06 01:02:52 +0800

    restructure layout

--- src/app/dashboard/layout.js
+++ src/app/dashboard/layout.js
@@ -1,12 +1,13 @@
 'use client'
 
-import { Avatar, Spinner } from '@chakra-ui/react'
+import { Avatar,  Spinner } from '@chakra-ui/react'
 import { signOut, useSession } from 'next-auth/react'
+import Image from 'next/image'
 import Link from 'next/link'
 import { useRouter } from 'next/navigation'
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 
-export default function layout({children}) {
+export default function Layout({children}) {
   const router = useRouter()
 
   const [open, setOpen] = useState(false)
@@ -24,22 +25,34 @@ export default function layout({children}) {
     if(status === 'unauthenticated'){
       return router.push('/signin')
     }
+
+    async function handleSignOut(){
+      const data = await signOut({callbackUrl: '/signin', redirect: false})
+      router.push(data.url)
+    }
   
-
- 
-
   return (
     <div className='bg-white flex w-full h-full relative'>
-      <div className={`absolute md:fixed ${open ? 'left-0' : 'left-[-1000px]'} md:left-0 w-[200px] h-full bg-slate-200 flex flex-col gap-3 items-center pt-4 transition-all ease-in-out duration-300 z-10`}>
-        <Link className='hover:bg-blue-500 transition-all ease-in-out duration-300 rounded-md p-3' href='/dashboard'>Dashboard</Link>
-        <div className='flex justify-end'>
-          {session && <button onClick={()=> signOut({callbackUrl: '/signin'})}>Sign Out</button>}
+      <div className={`absolute md:fixed ${open ? 'left-0' : 'left-[-1000px]'} overflow-x-hidden md:left-0 w-[200px] h-full bg-slate-200 flex flex-col justify-between py-[30px] items-center transition-all ease-in-out duration-300 z-10`}>
+        <div className='flex flex-col justify-center items-center'>
+          <div className='mb-[10px]'>
+          {session ? 
+            <Avatar name={session?.user?.username} size='lg' src='https://bit.ly/broken-link' /> : <Avatar/>}
+          </div>
+          <div className='flex p-[20px] w-full'>
+            <Image src='/dashboard.svg' width={24} height={24} alt='icon-dashboard'/>
+            <Link className='pl-[20px]' href='/dashboard'>Dashboard</Link>
+          </div>
         </div>
+        {session && <div className='flex justify-center p-[20px] w-full'>
+          <Image src='/log-out.svg' width={24} height={24} alt='icon-log-out'/>
+          <button className='pl-[20px]' onClick={handleSignOut}>Log Out</button>
+        </div>}
       </div>
       <div className='w-full h-full md:ml-[200px] bg-[#e7e4e426]'>
         <div className='bg-blue-500 w-full h-[50px] flex flex-col justify-center items-center'>
-          <div className='flex self-end pr-3'>
-            {session ? <Avatar onClick={()=> setOpen(open=> !open)} name={session?.user?.username} size='sm' src='https://bit.ly/broken-link' /> : <Link href='/signin'>Sign In</Link>}
+          <div className='flex self-end pr-3 md:hidden'>
+            <Image src='/menu.svg' width={24} height={24} alt='icon-log-out' onClick={()=> setOpen((open)=> !open)}/>
           </div>
         </div>
         {children}
