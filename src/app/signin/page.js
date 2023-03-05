6f91084fca26b4b4be781bb58c8dddf34f558fc0
reymooy27 <gdrrey@gmail.com>
2023-03-06 01:04:36 +0800

    retructure signin

--- src/app/signin/page.js
+++ src/app/signin/page.js
@@ -3,9 +3,11 @@
 import React, { useState } from 'react'
 import {FormLabel, Spinner} from '@chakra-ui/react'
 import { signIn } from 'next-auth/react'
+import Link from 'next/link'
+import { useRouter } from 'next/navigation'
 
-export default function page() {
-
+export default function Page() {
+  const router = useRouter()
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('') 
   const [isLoading, setIsLoading] = useState(false)
@@ -16,35 +18,45 @@ export default function page() {
     if(username === '' && password === '') return
     setIsLoading(true)
     e.preventDefault()
-    console.log(username, password)
-    signIn('credentials', {username, password})
-    .then((fullfiled) => {
-      setPassword('')
+    
+    const data = await signIn('credentials', {username, password, redirect: false, callbackUrl: '/dashboard'})
+
+    if(data.error){
+      setError(data.error)
       setIsLoading(false)
-    },(err)=>{
-      setError(err)
-    })
-    .catch(err=> setError(err))
+      setPassword('')
+    }
+   
+    if(data.ok){
+      router.push(data?.url)
+    }
   }
 
-  console.log(error)
   return (
     <>
       <div className='w-full h-full flex justify-center items-center gradient-bg'>
-        <form onSubmit={handleSignIn} className='px-6 py-6 rounded-md shadow-lg flex flex-col gap-4 w-[300px] mt-[-100px] _blur pb-[60px]'>
-          <div className='flex justify-center'>
-            <h1 className='text-[24px] fint-bold'>Sign In</h1>
+        <form onSubmit={handleSignIn} className='px-6 py-6 rounded-md flex flex-col gap-4 w-[300px] mt-[-100px] _blur pb-[60px] form'>
+          <div className='flex flex-col items-center'>
+            <h1 className='text-[24px] font-bold'>Sign In</h1>
+            {error && <p className='text-red-600'>{error}</p>}
           </div>
           <div>
-            <FormLabel>Username</FormLabel>
+            <div>
+              <FormLabel>Username</FormLabel>
+            </div>
             <input className='p-[10px] w-full bg-slate-200 opacity-[0.7] text-black h-[50px]' required placeholder='Username' value={username} onChange={(e)=> setUsername(e.target.value)}/>
           </div>
           <div className='mb-[10px]'>
-            <FormLabel>Password</FormLabel>
+            <div>
+              <FormLabel>Password</FormLabel>
+            </div>
             <input className='p-[10px] w-full bg-slate-200 opacity-[0.7] text-black h-[50px]'required placeholder='Password' type='password' value={password} onChange={(e)=> setPassword(e.target.value)}/>
           </div>
+          <div className='flex justify-center my-2'>
+            <Link href='/signup' className='text-[12px] underline'>Sign Up</Link>
+          </div>
           <button disabled={isLoading} type='submit' className={`${isLoading ? 'opacity-80' : 'opacity-100'} btn-grad flex items-center font-bold shadow-none`}>
-							{isLoading ? <Spinner size={'md'} /> : 'Submit'}</button>
+							{isLoading ? <div><Spinner size={'md'} /></div> : 'Submit'}</button>
         </form>
       </div>
     </>
