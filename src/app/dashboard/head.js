75cc75e1e3db942d8eb3af19b124a82d5081fc3d
reymooy27 <gdrrey@gmail.com>
2023-03-06 00:58:04 +0800

    fix typo 'crossOrigin'

--- src/app/dashboard/head.js
+++ src/app/dashboard/head.js
@@ -2,7 +2,7 @@ export default function Head() {
   return (
     <>
       <title>Dashboard</title>
-      <link rel="preload" href="/api/data" as="fetch" crossorigin="anonymous"></link>
+      <link rel="preload" href="/api/data" as="fetch" crossOrigin="anonymous"></link>
     </>
   )
 }
