aa63fbd94fb5016fe5e8b526fa9e106b4e7fdda9
reymooy27 <gdrrey@gmail.com>
2023-03-08 11:12:08 +0800

    create tamu with session

--- src/pages/api/create-tamu.js
+++ src/pages/api/create-tamu.js
@@ -1,10 +1,11 @@
 import prisma from "@/server/db/client";
-
+import {v4} from "uuid"
 export default async function handler(req, res) {
   const input = JSON.parse(req.body)
   if (req.method === 'POST') {
     try{
-      await prisma.tamu.create({
+
+      const tamu = await prisma.tamu.create({
         data:{
           nama: input.nama,
           alamat: input.alamat,
@@ -14,7 +15,23 @@ export default async function handler(req, res) {
           keperluan: input.keperluan
         }
       })
-      res.status(200).json('Berhasil')
+
+      const sessionToken = v4()
+      const expires = new Date(Date.now() + 30 * 60 * 1000)
+
+      const session = await prisma.tamuSession.create({
+        data: {
+          sessionToken,
+          expires,
+          tamu: {
+            connect: {
+              id: tamu.id,
+            },
+          },
+        },
+      })
+
+      res.status(200).json({tamu, session})
     }catch(err){
       console.log(err)
       res.status(400).json(err)
