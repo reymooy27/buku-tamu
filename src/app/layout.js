de1dfde05daa98302f6436821b464a786f65156f
reymooy27 <gdrrey@gmail.com>
2023-03-09 11:06:21 +0800

    delete inter font, error download

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,9 +4,6 @@ import './globals.css'
 import { CacheProvider } from '@chakra-ui/next-js'
 import { ChakraProvider } from '@chakra-ui/react'
 import { SessionProvider } from "next-auth/react"
-import { Inter } from '@next/font/google'
-
-const inter = Inter({ subsets: ['latin'] , variable: '--inter-font'})
 
 export default function RootLayout({ children, ...props }) {
 
