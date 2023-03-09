ecc3641cf8a5c59bc7df0210388d3c17b29a100a
reymooy27 <gdrrey@gmail.com>
2023-03-09 11:08:12 +0800

    styling

--- src/app/dashboard/layout.js
+++ src/app/dashboard/layout.js
@@ -33,30 +33,35 @@ export default function Layout({children}) {
   
   return (
     <div className='bg-white flex w-full h-full relative'>
-      <div className={`absolute md:fixed ${open ? 'left-0' : 'left-[-1000px]'} overflow-x-hidden md:left-0 w-[200px] h-full bg-slate-200 flex flex-col justify-between py-[30px] items-center transition-all ease-in-out duration-300 z-10`}>
+      <div className={`absolute md:fixed ${open ? 'left-0' : 'left-[-1000px]'} md:left-0 w-[200px] h-full bg-slate-200 flex flex-col justify-between py-[30px] items-center transition-all ease-in-out duration-300 z-10`}>
         <div className='flex flex-col justify-center items-center'>
           <div className='mb-[10px]'>
           {session ? 
             <Avatar name={session?.user?.username} size='lg' src='https://bit.ly/broken-link' /> : <Avatar/>}
           </div>
           <div className='flex p-[20px] w-full'>
-            <Image src='/dashboard.svg' width={24} height={24} alt='icon-dashboard'/>
-            <Link className='pl-[20px]' href='/dashboard'>Dashboard</Link>
+            <Link className='pl-[20px] flex gap-3 p-4 hover:bg-slate-600 rounded-[10px] transition-all duration-300 ease-in-out' href='/dashboard'>
+              <Image src='/dashboard.svg' width={24} height={24} alt='icon-dashboard'/>
+              <span>Dashboard</span>
+            </Link>
           </div>
         </div>
         {session && <div className='flex justify-center p-[20px] w-full'>
+          
+          <button className='pl-[20px] flex gap-3 p-4 hover:bg-slate-600 rounded-[10px] transition-all duration-300 ease-in-out' onClick={handleSignOut}>
           <Image src='/log-out.svg' width={24} height={24} alt='icon-log-out'/>
-          <button className='pl-[20px]' onClick={handleSignOut}>Log Out</button>
+            <span>Log Out</span>
+          </button>
         </div>}
       </div>
-      <div className='w-full h-full md:ml-[200px] bg-[#e7e4e426]'>
+      <div className='w-full md:w-[calc(100%-200px)] h-full absolute md:left-[200px] bg-[#e7e4e426]'>
         <div className='bg-blue-500 w-full h-[50px] flex flex-col justify-center items-center'>
           <div className='flex self-end pr-3 md:hidden'>
             <Image src='/menu.svg' width={24} height={24} alt='icon-log-out' onClick={()=> setOpen((open)=> !open)}/>
           </div>
         </div>
         {children}
-        </div>
+      </div>
     </div>
   )
 }
