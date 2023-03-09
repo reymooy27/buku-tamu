82c41914b76a5700eedd46dedd6824030f7c4d4c
reymooy27 <gdrrey@gmail.com>
2023-03-09 11:07:20 +0800

    add preload fetch

--- src/app/dashboard/head.js
+++ src/app/dashboard/head.js
@@ -2,7 +2,11 @@ export default function Head() {
   return (
     <>
       <title>Dashboard</title>
-      <link rel="preload" href="/api/data" as="fetch" crossOrigin="anonymous"></link>
+      <link rel="preload" href="/api/get-tamu-all-count" as="fetch" crossOrigin="anonymous"></link>
+      <link rel="preload" href="/api/get-tamu-by-date-count" as="fetch" crossOrigin="anonymous"></link>
+      <link rel="preload" href="/api/get-tamu-by-month-count" as="fetch" crossOrigin="anonymous"></link>
+      <link rel="preload" href="/api/get-tamu-by-year-count" as="fetch" crossOrigin="anonymous"></link>
+      <link rel="preload" href="/api/get-monthly-count" as="fetch" crossOrigin="anonymous"></link>
     </>
   )
 }
